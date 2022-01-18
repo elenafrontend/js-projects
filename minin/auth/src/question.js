43036878a3c5e138ef51002d34505760c3a19385
Elena Gumerova <elenafrontend@ya.ru>
2022-01-18 23:10:58 +0300

    feat: render question list

--- minin/auth/src/question.js
+++ minin/auth/src/question.js
@@ -9,7 +9,41 @@ export class Question {
     })
       .then(response => response.json())
       .then(response => {
-        console.log(response)
+        question.id = response.name;
+        return question;
       })
+      .then(addToLocalStorage)
+      .then(Question.renderList)
   }
+
+  static renderList() {
+    const questions = getQuestionsFromLocalStorage();
+    const listHtml = questions.length
+      ? questions.map(makeQuestionCard).join('')
+      : `<div class="mui&#45;&#45;text-headline">you haven't asked the question yet</div>`;
+
+    const list = document.getElementById('list');
+    list.innerHTML = listHtml;
+  }
+}
+
+function addToLocalStorage(question) {
+  const questions = getQuestionsFromLocalStorage();
+  questions.push(question);
+  localStorage.setItem('questions', JSON.stringify(questions));
+}
+
+function getQuestionsFromLocalStorage() {
+  return JSON.parse(localStorage.getItem('questions') || '[]');
+}
+
+function makeQuestionCard(question) {
+  return `
+    <div class="mui--text-black-54">
+        ${new Date(question.date).toLocaleDateString()}
+        ${new Date(question.date).toLocaleTimeString()}
+    </div>
+    <div>${question.text}</div>
+    <br>
+  `;
 }
