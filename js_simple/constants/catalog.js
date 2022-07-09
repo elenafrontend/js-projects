c89b5c849e0e670d96b3a748b7904e8e0bcc37ff
Elena Gumerova <elenafrontend@ya.ru>
2022-07-09 21:53:47 +0300

    feat: catalog data

---
+++ js_simple/constants/catalog.js
@@ -0,0 +1,44 @@
+const CATALOG = [
+  {
+    id: 'el1',
+    name: 'Ноутбук HP 17-cp0126ur 5D666EA',
+    img: 'https://github.com/elenafrontend/js-projects/blob/js_simple/js_simple/img/HP_17-cp0126ur_5D666EA.webp',
+    price: 54999
+  },
+  {
+    id: 'el2',
+    name: 'Ноутбук Apple MacBook Air 13 M1/16/1TB SSD Gold (Z12B)',
+    img: 'https://github.com/elenafrontend/js-projects/blob/js_simple/js_simple/img/Apple_MacBook_Air_13.webp',
+    price: 169999
+  },
+  {
+    id: 'el3',
+    name: 'Ноутбук HUAWEI MateBook 14S HKD-W76 16+1TB Space Grey',
+    img: 'https://github.com/elenafrontend/js-projects/blob/js_simple/js_simple/img/HUAWEI_MateBook_14S_HKD-W76.webp',
+    price: 119999
+  },
+  {
+    id: 'el4',
+    name: 'Ноутбук ASUS VivoBook 15 X513EA-BQ2445X',
+    img: 'https://github.com/elenafrontend/js-projects/blob/js_simple/js_simple/img/ASUS_VivoBook_15_X513EA-BQ2445X.webp',
+    price: 64999
+  },
+  {
+    id: 'el5',
+    name: 'Ноутбук ASUS Zenbook 13 OLED UX325EA-KG703WX',
+    img: 'https://github.com/elenafrontend/js-projects/blob/js_simple/js_simple/img/ASUS_Zenbook_13_OLED_UX325EA-KG703W.webp',
+    price: 64999
+  },
+  {
+    id: 'el6',
+    name: 'Ноутбук для бизнеса ASUS ExpertBook B9 B9400CEA-KC0116T',
+    img: 'https://github.com/elenafrontend/js-projects/blob/js_simple/js_simple/img/ASUS_ExpertBook_B9 B9400CEA-KC0116T.webp',
+    price: 64999
+  },
+  {
+    id: 'el7',
+    name: 'Ноутбук игровой HP Victus 16-e0145u',
+    img: 'https://github.com/elenafrontend/js-projects/blob/js_simple/js_simple/img/HP_Victus_16-e0145u B9400CEA-KC0116T.webp',
+    price: 64999
+  },
+]
